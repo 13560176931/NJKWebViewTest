5808a690e6a0950ff124c61628265c4710ebc988
郑乾坤 <Devilan@zhengqiankundemacbook-pro.local>
2016-01-09 15:19:51 +0800

    Initial Commit

---
+++ NJKWebViewTestTests/NJKWebViewTestTests.m
@@ -0,0 +1,39 @@
+//
+//  NJKWebViewTestTests.m
+//  NJKWebViewTestTests
+//
+//  Created by 郑乾坤 on 16/1/9.
+//  Copyright © 2016年 郑乾坤. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NJKWebViewTestTests : XCTestCase
+
+@end
+
+@implementation NJKWebViewTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
